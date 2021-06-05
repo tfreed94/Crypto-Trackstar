3c858ba7af488cc073b3bd94903a020585123cf4
Troy Freed <tfreed94@gmail.com>
2021-06-05 17:42:47 -0400

    updated connection and server

--- server.js
+++ server.js
@@ -1,24 +1,33 @@
+const path = require('path');
 const express = require('express');
-const routes = require('./routes');
-const sequelize = require('./config/connection');
-const coinGeckoAPI = require('coingecko-api');
-const handleBars = require('handlebars')
-const exphbs = require('express-handlebars');
-const helpers = require('./utils/helpers');
-const routes = require('./controllers');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
+const hbs = exphbs.create({ helpers });
+const sess = {
+    secret: process.env.secret,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
+app.use(session(sess));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static('public'));
+app.use(express.static('assets'));
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-sequelize.sync({ force: false })
-.then(() => {
-  app.listen(PORT, () => 
-  console.log(`App listening on port ${PORT}!`));
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening to port ' + PORT));
 });
